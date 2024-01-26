c32775b955ed6b19c8c8811e8b2d25f7bd2cfd7d
정창휘 <jch9937@gmail.com>
2024-01-26 15:00:56 +0900

    feat: 랭킹페이지 구현

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,5 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Ranking from './pages/ranking/Ranking';
+
+
 function App() {
-  return <div>처음</div>;
+  return (
+    <BrowserRouter>
+      <>
+        <div>처음</div>
+        <Routes>
+          <Route path="/ranking" element={<Ranking />} />
+        </Routes>
+      </>
+    </BrowserRouter>
+  );
 }
 
 export default App;
+
