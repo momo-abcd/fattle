a35b44813904cadb15fc6f705f2907c2b50324f6
진성민 <sengmin14@naver.com>
2024-01-26 14:37:46 +0900

    feat: 로그인페이지 및 회원가입 페이지 구성

---
+++ FE/src/pages/join/Join.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+const Join = () => {
+  let [name, setName] = useState;
+  return (
+    <div>
+      <h3>닉네임 입력</h3>
+      <input type="text"></input>
+      <button onClick={() => {}}>확인</button>
+    </div>
+  );
+};
+
+export default Join;
