4879ceac0e45f785562c0a5a29910755810d32cc
김정민 <kjm1994@naver.com>
2024-01-29 08:56:58 +0900

    style: 사용하지 않는 import 제거

--- BE/src/main/java/com/sixman/fattle/api/controller/OAuthController.java
+++ BE/src/main/java/com/sixman/fattle/api/controller/OAuthController.java
@@ -2,14 +2,11 @@ package com.sixman.fattle.api.controller;
 
 import com.sixman.fattle.api.service.OAuthService;
 import com.sixman.fattle.dto.response.LoginResponse;
-import com.sixman.fattle.dto.response.OAuthTokenResponse;
-import com.sixman.fattle.entity.User;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequestMapping("/oauth")
