d438392d6789b515fad2ccde61d62bfcb34ada99
진성민 <sengmin14@naver.com>
2024-01-29 09:00:21 +0900

    docs: 최근 커밋으로 돌리기

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Main from './pages/main/Main';
 import Login from './pages/login/Login';
@@ -6,15 +5,6 @@ import Join from './pages/join/Join';
 
 function App() {
   let isLogged = false;
-=======
-import { Routes, Route } from 'react-router-dom';
-
-import Join from './pages/join/Join';
-import Auth from './utils/Auth';
-
-function App() {
-  let isLogged = true;
->>>>>>> Stashed changes
   return (
     <Routes>
       <Route path="/" element={<Auth isLogged={isLogged} />} />
