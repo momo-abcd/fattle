da294ee4ab3cece3c16e0c4d2ab60e5003071c2d
정창휘 <jch9937@gmail.com>
2024-01-29 09:00:48 +0900

    feature: 랭킹페이지, 경험치 조회, 캐릭터 조회

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Ranking from './pages/ranking/Ranking';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Ranking from './pages/ranking/Ranking'
+import Character from './components/main/Character'
+import ExpHistory from './components/main/ExpHistory';
+
 
 
 function App() {
   return (
     <BrowserRouter>
-      <>
-        <div>처음</div>
         <Routes>
+          <Route path="/" element={<Character />} />
+          <Route path="/history" element={<ExpHistory />} />
           <Route path="/ranking" element={<Ranking />} />
         </Routes>
-      </>
     </BrowserRouter>
-  );
+  )
 }
 
 export default App;
+
 
