7eea15d5afcb0ec5913d1e4353b54e6ec259ca31
양윤모 <yym4481@naver.com>
2024-01-31 16:57:03 +0900

    배포전 주소설정

--- FE/src/config.js
+++ FE/src/config.js
@@ -1,6 +1,6 @@
 // const BASE_URL = 'https://www.i10E106.p.ssafy.io';
-// const BASE_URL = 'http://i10e106.p.ssafy.io:7979';
-const BASE_URL = 'http://localhost:7979';
+const BASE_URL = 'http://i10e106.p.ssafy.io:8000';
+// const BASE_URL = 'http://localhost:7979';
 
 export const API = {
   MYPAGE_GET: `${BASE_URL}/user/mypage/`, // get + put
