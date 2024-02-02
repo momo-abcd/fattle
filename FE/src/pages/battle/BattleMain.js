d17ed1a59ad79eaacd1fc277794637c1e28f13e8
양윤모 <yym4481@naver.com>
2024-02-02 19:04:53 +0900

    feat: Battle 컴포넌트생성

---
+++ FE/src/pages/battle/BattleMain.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../../commons/Footer';
+
+const BattleMain = (props) => {
+  return (
+    <>
+      배틀페이지메인
+      <Footer />
+    </>
+  );
+};
+
+export default BattleMain;
---
+++ FE/src/pages/battle/battleRoutes.js
@@ -0,0 +1,4 @@
+import { Route } from 'react-router-dom';
+import BattleMain from './BattleMain';
+
+export default [<Route path="" element={<BattleMain />} key="main" />];
