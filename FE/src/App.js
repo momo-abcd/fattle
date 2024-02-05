a20fe3bea12bfb5c562ae0e3f54d2829579f18a3
진성민 <sengmin14@naver.com>
2024-02-06 08:41:42 +0900

    feat: 유저코드/회원가입페이지 이동 여부 boolean -> userCode로 변경

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,6 +1,8 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import JoinPage1 from './pages/join/JoinPage1';
 import JoinPage2 from './pages/join/JoinPage2.js';
+import JoinPage3 from './pages/join/JoinPage3.js';
+import JoinPage4 from './pages/join/JoinPage4.js';
 import Mypage from './pages/mypage/Mypage';
 import Auth from './utils/Auth';
 import Ranking from './pages/ranking/Ranking';
@@ -24,6 +26,8 @@ function App() {
         <Route path="/login" element={<Auth isLogged={isLogged} />} />
         <Route path="/join/page1" element={<JoinPage1></JoinPage1>}></Route>
         <Route path="/join/page2" element={<JoinPage2></JoinPage2>}></Route>
+        <Route path="/join/page3" element={<JoinPage3></JoinPage3>}></Route>
+        <Route path="/join/page4" element={<JoinPage4></JoinPage4>}></Route>
         <Route path="/login-callback" element={<Callback />}></Route>
         <Route path="/main" element={<Main />} />
         <Route path="/history" element={<ExpHistory />} />
