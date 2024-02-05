a90e4037f4afe9a5506ff0485a8b9b9a1d14f1a3
진성민 <sengmin14@naver.com>
2024-02-05 14:10:46 +0900

    feat: 로그인 여부에 따라 회원가이페이지 or 메인페이지로 Navigate처리

--- FE/src/utils/Auth.js
+++ FE/src/utils/Auth.js
@@ -1,10 +1,13 @@
 import { Navigate } from 'react-router-dom';
-
+import { useSelector } from 'react-redux';
 import Main from './../pages/main/Main';
 import Login from './../pages/login/Login';
 
 const Auth = (props) => {
-  if (props.isLogged) {
+  const userCode = useSelector((state) => {
+    return state.userCode;
+  });
+  if (userCode !== 0) {
     return (
       <div>
         <Main />
