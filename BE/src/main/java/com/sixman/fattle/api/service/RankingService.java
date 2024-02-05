76bc82e14a812f7c1d337280340f4a7d9de322b6
강주원 <wizuded@naver.com>
2024-02-05 10:46:06 +0900

    Refactor: 쓰지 않는 코드 삭제

--- BE/src/main/java/com/sixman/fattle/api/service/RankingService.java
+++ BE/src/main/java/com/sixman/fattle/api/service/RankingService.java
@@ -1,6 +1,5 @@
 package com.sixman.fattle.api.service;
 
-import com.sixman.fattle.dto.dto.RankingInfo;
 import com.sixman.fattle.dto.response.RankingListResponse;
 import org.springframework.http.ResponseEntity;
 
