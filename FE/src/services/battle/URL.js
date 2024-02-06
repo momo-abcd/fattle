5043b4c9dabd8c33eccd2a2d4eff619782f6b9fd
양윤모 <yym4481@naver.com>
2024-02-06 11:24:16 +0900

    feat: 배틀페이지 api 변수명 추가

---
+++ FE/src/services/battle/URL.js
@@ -0,0 +1,16 @@
+import BASE_URL from '../../config';
+const API = {
+  BATTLE_CREATE_POST: `${BASE_URL}/battle/create/`,
+  BATTLE_FINISH_GET: `${BASE_URL}/battle/finish/`,
+  BATTLE_INFO_GET: `${BASE_URL}/battle/info/`,
+  BATTLE_LIST_GET: `${BASE_URL}/battle/list/`,
+  BATTLE_MODIFY_PLAYER_PATCH: `${BASE_URL}/battle/player/`,
+  BATTLE_DELETE_PLAYER_DELETE: `${BASE_URL}/battle/player/`,
+  BATTLE_REGIST_PLAYER_POST: `${BASE_URL}/battle/regist/player`,
+  BATTLE_REGIST_TRIGGER_POST: `${BASE_URL}/battle/regist/trigger`,
+  BATTLE_SETTING_PATCH: `${BASE_URL}/battle/setting`,
+  BATTLE_START_GET: `${BASE_URL}/battle/start/`,
+  BATTLE_DELETE_TRIGGER_DELETE: `${BASE_URL}/battle/trigger/`,
+  BATTLE_WEIGHT_PATCH: `${BASE_URL}/battle/weight`,
+};
+export default API;
