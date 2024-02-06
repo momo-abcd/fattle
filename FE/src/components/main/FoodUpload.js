d066a9150ddd3d1f6bb7b81492e44c530226cbef
정창휘 <jch9937@gmail.com>
2024-02-06 09:04:13 +0900

    tmp: 식단 등록 및 카메라 기능 구현

--- FE/src/components/main/FoodUpload.js
+++ FE/src/components/main/FoodUpload.js
@@ -1,31 +1,41 @@
 import React from "react";
+import { Route, Routes, Link } from "react-router-dom";
 import styles from "../../styles/main/FoodUpload.module.css";
 import uploadbutton from "../../assets/images/main/uploadbutton.svg";
 import { API } from '../../services/main/URL';
+import FoodRegister from "./FoodRegister";
 
 function FoodUpload() {
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        <div className={styles.box}>
-          <span className={styles.label1}>아침</span>
-          <img src={uploadbutton} alt='' className={styles.positionAbsolute}/>
-        </div>
-        <div className={styles.box}>
-          <span className={styles.label2}>점심</span>
-          <img src={uploadbutton} alt='' className={styles.positionAbsolute}/>
-        </div>
-        <div className={styles.box}>
-          <span className={styles.label3}>저녁</span>
-          <img src={uploadbutton} alt='' className={styles.positionAbsolute}/>
-        </div>
-        <div className={styles.box}>
-          <span className={styles.label4}>간식</span>
-          <img src={uploadbutton} alt='' className={styles.positionAbsolute}/>
-        </div>
+          <div className={styles.box}>
+            <span className={styles.label1}>아침</span>
+            <Link to="/foodupload">
+              <img src={uploadbutton} alt='' className={styles.positionAbsolute}/>
+            </Link>
+          </div>
+          <div className={styles.box}>
+            <span className={styles.label2}>점심</span>
+            <Link to="/foodupload">
+              <img src={uploadbutton} alt='' className={styles.positionAbsolute}/>
+            </Link>
+          </div>
+          <div className={styles.box}>
+            <span className={styles.label3}>저녁</span>
+            <Link to="/foodupload">
+              <img src={uploadbutton} alt='' className={styles.positionAbsolute}/>
+            </Link>
+          </div>
+          <div className={styles.box}>
+            <span className={styles.label4}>간식</span>
+            <Link to="/foodupload">
+              <img src={uploadbutton} alt='' className={styles.positionAbsolute}/>
+            </Link>
+          </div>
       </div>
     </div>
   );
 }
 
-export default FoodUpload;
+export default FoodUpload
