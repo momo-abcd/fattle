b3933cf3a2b393b3815ec3759c2062ff98897e07
진성민 <sengmin14@naver.com>
2024-02-07 17:26:32 +0900

    feat: 키/몸무게 수정 페이지에 최초 데이터 현재 키/몸무게 가지고 오도록 수정

--- FE/src/components/main/BodyinfoModify.js
+++ FE/src/components/main/BodyinfoModify.js
@@ -5,8 +5,8 @@ import API from '../../services/main/URL';
 import styles from '../../styles/main/BodyinfoModify.module.css';
 import { useSelector } from 'react-redux';
 const BodyinfoModify = () => {
-  const [height, setHeight] = useState('');
-  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState(0);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const userCode = useSelector((state) => {
     return state.userCode;
@@ -30,7 +30,10 @@ const BodyinfoModify = () => {
     }, []);
 
     try {
-      const apiUrl = `${API.USER_GET}`;
+      const apiUrl = `${API.USER_GET}${userCode}`;
+      {
+        console.log('asd');
+      }
       const data = {
         height: height,
         weight: weight,
@@ -59,7 +62,15 @@ const BodyinfoModify = () => {
       <img
         src={Frame2}
         alt="Click to open modal"
-        onClick={openModal}
+        onClick={() => {
+          axios.get(`${API.USER_GET}${userCode}`).then((res) => {
+            console.log(res.data.height);
+            setHeight(res.data.height);
+            console.log(res.data.weight);
+            setWeight(res.data.weight);
+          });
+          openModal();
+        }}
         className={styles.imgStyle}
       />
 
