5332cd0ad69703a132ef5e52eb3558c39ed8ef93
김정민 <kjm1994@naver.com>
2024-02-07 14:24:04 +0900

    refactor: 사용하지 않는 코드 제거

--- BE/src/main/java/com/sixman/fattle/config/SecurityConfig.java
+++ BE/src/main/java/com/sixman/fattle/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import com.sixman.fattle.utils.AuthenticatedMatchers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -50,7 +49,6 @@ public class SecurityConfig {
 				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
 				.build();
 	}
-
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
--- BE/src/main/java/com/sixman/fattle/config/SwaggerConfig.java
+++ BE/src/main/java/com/sixman/fattle/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package com.sixman.fattle.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -9,14 +8,6 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
-//@OpenAPIDefinition(
-//        info = @Info(
-//                title = "FATTLE API",
-//                description = "FATTLE API DOCS",
-//                version = "v1"
-//        )
-//)
 @Configuration
 public class SwaggerConfig {
 
