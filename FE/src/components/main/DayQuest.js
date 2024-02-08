6972c317c623f81ba9ce46425831f9358e3f355d
정창휘 <jch9937@gmail.com>
2024-02-08 11:01:02 +0900

    feat: 일일퀘스트 기능 수정중

--- FE/src/components/main/DayQuest.js
+++ FE/src/components/main/DayQuest.js
@@ -1,93 +1,125 @@
-import React, { useState, useEffect } from "react";
-import styles from "../../styles/main/DayQuest.module.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styles from '../../styles/main/DayQuest.module.css';
+import API from '../../services/main/URL';
+import { useSelector } from 'react-redux';
+import checkImg from '../../assets/images/main/check.svg';
+import notCheckImg from '../../assets/images/main/notcheck.svg';
+
+// 각 운동 코드와 설명을 매핑하는 객체
+const exerciseDescriptions = {
+  run: '러닝 30분',
+  pus: '팔굽혀 펴기 50회',
+  bur: '버피 테스트 20회',
+  pla: '플랭크 1분',
+  squ: '스쿼트 20개',
+  pul: '턱걸이 10개',
+};
 
 function DayQuest() {
-  const [completedCount, setCompletedCount] = useState(0);
-  const [questList, setQuestList] = useState(Array(10).fill(false));
+  const [completedCount, setCompletedCount] = useState(1);
+  const [questList, setQuestList] = useState([]);
   const [lastVisitedDate, setLastVisitedDate] = useState(null);
+  const [selectedBars, setSelectedBars] = useState([]);
+  const userCode = useSelector((state) => {
+    return state.userCode;
+  });
 
   useEffect(() => {
-    // 로컬 스토리지에서 저장된 완료된 퀘스트 정보와 날짜 정보를 가져옴
-    const storedQuestList = JSON.parse(localStorage.getItem("questList")) || Array(10).fill(false);
-    const storedDate = localStorage.getItem("lastVisitedDate");
-
-    if (storedDate) {
-      setLastVisitedDate(new Date(storedDate));
-    } else {
-      // 날짜 정보가 없으면 초기화
-      setLastVisitedDate(new Date());
-      setQuestList(Array(10).fill(false));
-      setCompletedCount(0);
-      localStorage.setItem("questList", JSON.stringify(Array(10).fill(false)));
-      localStorage.setItem("lastVisitedDate", new Date().toString());
-    }
-
-    setQuestList(storedQuestList);
-    const newCompletedCount = storedQuestList.filter((quest) => quest).length;
-    setCompletedCount(newCompletedCount);
+    axios
+      .get(`${API.QUEST_LIST_GET}${userCode}`)
+      .then((response) => {
+        setQuestList(response.data);
+      })
+      .catch((error) => {
+        console.error('데이터를 불러오는 데 실패했습니다:', error.message);
+      });
   }, []);
 
-  const handleQuestClick = (index) => {
-    if (completedCount === 10) {
-      return; // 7개까지만 클릭 가능하도록 막음
-    }
+  const handleQuestClick = (index, value) => {
+    const isSelected = selectedBars.includes(index);
+    const newValue = isSelected ? -value : value;
 
-    setQuestList((prevQuestList) => {
-      const newQuestList = [...prevQuestList];
-      newQuestList[index] = !newQuestList[index];
-
-      // 완료된 퀘스트 개수 계산
-      const newCompletedCount = newQuestList.filter((quest) => quest).length;
-      setCompletedCount(newCompletedCount);
-
-      // 로컬 스토리지에 완료된 퀘스트 정보와 날짜 정보 저장
-      localStorage.setItem("questList", JSON.stringify(newQuestList));
-      localStorage.setItem("lastVisitedDate", new Date().toString());
-
-      return newQuestList;
+    setSelectedBars((prevSelectedBars) => {
+      if (isSelected) {
+        return prevSelectedBars.filter((barIndex) => barIndex !== index);
+      } else {
+        return [...prevSelectedBars, index];
+      }
     });
+
+    // 퀘스트 클릭 시 해당 값을 서버에 업데이트
+    axios
+      .post(`${API.UPDATE_QUEST_POST}`, {
+        userCode: `${userCode}`,
+        exercise: 'RUN',
+      })
+      .then((response) => {
+        // 성공적으로 업데이트된 경우에만 로컬 상태 업데이트
+        setCompletedCount((prevCount) => prevCount + newValue);
+      })
+      .catch((error) => {
+        console.error('퀘스트를 업데이트하는 데 실패했습니다:', error.message);
+      });
   };
-
-  const questNames = [
-    "출석",
-    "운동1", "운동2", "운동3", "운동4", "운동5", "운동6",
-    "아침식단", "점심식단", "저녁식단"
-  ];
-
-  // 자정에 퀘스트 초기화
-  useEffect(() => {
-    const midnight = new Date();
-    midnight.setHours(24, 0, 0, 0);
-
-    const timeUntilMidnight = midnight - new Date();
-
-    const timeout = setTimeout(() => {
-      setLastVisitedDate(new Date());
-      setQuestList(Array(10).fill(false));
-      setCompletedCount(0);
-      localStorage.setItem("questList", JSON.stringify(Array(10).fill(false)));
-      localStorage.setItem("lastVisitedDate", new Date().toString());
-    }, timeUntilMidnight);
-
-    return () => clearTimeout(timeout);
-  }, []);
 
   return (
     <div className={styles.dayQuestContainer}>
       <h3 className={styles.questTitle}>일일퀘스트</h3>
 
       <div className={styles.exerciseListContainer}>
-        <ul className={styles.exerciseList}>
-          {questNames.map((questName, index) => (
-            <li
-              key={index}
-              onClick={() => handleQuestClick(index)}
-              className={`${styles.exerciseItem} ${questList[index] ? styles.exerciseItemSelected : styles.exerciseItemUnselected}`}
+        <div
+          className={`${styles.exerciseItem} ${
+            questList.foodCnt > 0
+              ? styles.exerciseItemSelected
+              : styles.exerciseItemUnselected
+          }`}
+          onClick={() => handleQuestClick(-1, questList.foodCnt)}
+        >
+          <p>건강한 식단 먹기: {questList.foodCnt}</p>
+          <img
+            src={questList.foodCnt > 0 ? checkImg : notCheckImg}
+            alt="Check icon"
+            className={styles.checkImage}
+          />
+        </div>
+        <div
+          className={`${styles.exerciseItem} ${
+            questList.dayCheck === 1
+              ? styles.exerciseItemSelected
+              : styles.exerciseItemUnselected
+          }`}
+          onClick={() => handleQuestClick(-2, questList.dayCheck)}
+        >
+          <p>출석 체크: {questList.dayCheck}</p>
+          <img
+            src={questList.dayCheck === 1 ? checkImg : notCheckImg}
+            alt="Check icon"
+            className={styles.checkImage}
+          />
+        </div>
+        {/* exercise의 각 항목 출력 */}
+        {questList.exercise &&
+          Object.entries(questList.exercise).map(([key, value], index) => (
+            <div
+              key={key}
+              className={`${styles.exerciseItem} ${
+                value === 1
+                  ? styles.exerciseItemSelected
+                  : styles.exerciseItemUnselected
+              }`}
+              onClick={() => handleQuestClick(index, value)}
             >
-              <span style={{ alignSelf: "center" }}>{questName}</span>
-            </li>
+              <p>
+                {exerciseDescriptions[key]}: {value}
+              </p>
+              <img
+                src={value === 1 ? checkImg : notCheckImg}
+                alt="Check icon"
+                className={styles.checkImage}
+              />
+            </div>
           ))}
-        </ul>
       </div>
 
       {/* 완료된 퀘스트 개수가 7개까지만 표시 */}
@@ -101,14 +133,3 @@ function DayQuest() {
 }
 
 export default DayQuest;
-
-
-
-
-
-
-
-
-
-
-
