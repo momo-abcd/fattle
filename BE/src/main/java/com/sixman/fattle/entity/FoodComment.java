10e1c7d4dee1eb8e482f0c89897f1eac316ad84e
김정민 <kjm1994@naver.com>
2024-02-13 15:33:18 +0900

    fix: food comment regist 오류 수정

--- BE/src/main/java/com/sixman/fattle/entity/FoodComment.java
+++ BE/src/main/java/com/sixman/fattle/entity/FoodComment.java
@@ -17,7 +17,7 @@ import java.time.LocalDateTime;
 public class FoodComment {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "food_comment_cd")
     private int foodCommentCd;
 
