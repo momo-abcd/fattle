11f69957de74a6b88b49b2e65b4f0e430052a4e9
진성민 <sengmin14@naver.com>
2024-02-14 14:43:47 +0900

    feat: style: 로그인 페이지 css적용

--- FE/src/pages/login/Login.js
+++ FE/src/pages/login/Login.js
@@ -1,4 +1,5 @@
-import './Login.css';
+// import './Login.css';
+import styles from '../../styles/login/Login.module.css';
 // import KakaoBtn from '../../assets/login/kakao_btn';
 import { useDispatch, useSelector } from 'react-redux';
 import kakao_btn from '../../assets/login/kakao_btn.png';
@@ -6,16 +7,20 @@ import { API } from '../../services/login/URL';
 const Login = () => {
   return (
     <div>
-      <div className="background">
-        <p className="LoginText">ㅤ</p>
-        <div className="wrapper">
-          <div className="title">
-            <div className="title_font">FATTLE</div>
+      <div className={styles.background}>
+        <p className={styles.LoginText}>ㅤ</p>
+        <div className={styles.wrapper}>
+          <div className={styles.title}>
+            <div className={styles.title_font}>FATTLE</div>
+            <div className={styles.content_font}>
+              친구와의 배틀을 통해
+              <br></br>
+              캐릭터와 함께 성장하는 다이어트
+            </div>
           </div>
-
-          <div className="kakao">
-            <a
-              className="kakaoBtn"
+          <div className={styles.kakao}>
+            <div
+              className={styles.kakaoBtn}
               type="button"
               onClick={(e) => {
                 e.preventDefault();
@@ -23,13 +28,16 @@ const Login = () => {
                 window.location.href = `${API.LOGIN_GET}`;
               }}
             >
-              <img className="kakao" src={kakao_btn} alt="Kakao Button" />
-            </a>
+              <img
+                className={styles.kakao}
+                src={kakao_btn}
+                alt="Kakao Button"
+              />
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
 };
-
 export default Login;
