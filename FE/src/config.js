40958c91d7beb0e6c69f2f51116dea4077923330
양윤모 <yym4481@naver.com>
2024-02-14 10:56:30 +0900

    fix: config.js BASE_URL 서버주소로 수정

--- FE/src/config.js
+++ FE/src/config.js
@@ -1,4 +1,2 @@
-// const BASE_URL = 'http://localhost:8000';
-const BASE_URL = 'http://localhost:8000/api';
-// const BASE_URL = 'http://i10e106.p.ssafy.io:8000';
+const BASE_URL = 'https://i10e106.p.ssafy.io/api';
 export default BASE_URL;
