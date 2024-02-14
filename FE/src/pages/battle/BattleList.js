195a75c81192a0fdfedc4aff6c78244565408e4a
정창휘 <jch9937@gmail.com>
2024-02-14 09:26:17 +0900

    style: 배틀페이지 스타일 수정

--- FE/src/pages/battle/BattleList.js
+++ FE/src/pages/battle/BattleList.js
@@ -12,10 +12,18 @@ import Footer from '../../commons/Footer';
 import { Link } from 'react-router-dom';
 import BackHeader from '../../components/commons/BackHeader.js';
 import BattleStyles from '../../styles/battle/Battle.module.css';
+import profileImg from '../../assets/svg/mypage/ProfileImg.svg';
+import trashCan from '../../assets/svg/battle/trashCan.svg';
+import classification from '../../assets/svg/battle/classfication.svg';
+import liveImg from '../../assets/svg/battle/liveImg.svg';
+import Invite from '../../assets/svg/battle/battleInvite.svg';
+
 const BattleList = (props) => {
   const userCode = useSelector((state) => state.userCode);
   const navigate = useNavigate();
   const [data, setData] = useState(null);
+  const [activeTab, setActiveTab] = useState('progress'); // progress: 진행중, end: 종료
+
   useEffect(() => {
     (async () => {
       console.log(userCode);
@@ -32,121 +40,189 @@ const BattleList = (props) => {
     if (res.status !== 200) navigate('/login');
     console.log(res.data);
     setData(res.data.list);
-    // setData(res.data);
-    // navigate('create', { state: { battleCode: res.data.code } });
   };
-  // 배틀 삭제
+
   const onDeleteBattle = async (battleCode) => {
     const res = await deleteBattle(battleCode);
     if (res.status === 200) {
-      // 삭제가 성공한 경우, 배틀 목록 다시 불러오기
       const updatedBattleList = await getBattleList(userCode);
       setData(updatedBattleList.data.list);
+      navigate('/battle');
     } else {
       console.error('배틀 삭제 실패');
     }
   };
 
   const handleBattleItemClick = (item) => {
-    // item이 유효한지 확인
     if (!item) {
       console.error('Item is not valid');
       return;
     }
 
-    // item.playerList가 정의되어 있고 유효한 경우에만 사용자 확인
     const isUserInPlaylist =
       item.playerList &&
       item.playerList.some((player) => player.userCode === userCode);
-    // item.triggerList가 정의되어 있고 유효한 경우에만 사용자 확인
     const isUserInTriggerList =
       item.triggerList &&
       item.triggerList.some((player) => player.userCode === userCode);
 
-    // 사용자의 userCode가 포함되어 있으면 battle/detail 페이지로 이동, 아니면 battle/code 페이지로 이동
-    let destination = '/battle/code'; // 기본적으로는 코드 페이지로 이동
+    let destination = '/battle/code';
     if (isUserInPlaylist || isUserInTriggerList) {
-      destination = '/battle/code'; // 사용자가 플레이어 목록이나 트리거 목록에 포함된 경우 디테일 페이지로 이동
+      destination = '/battle/detail';
     }
 
     navigate(destination, { state: { battleCode: item.battleCode } });
   };
+
   return (
     <>
-      <BackHeader navigate={navigate} />
-      <div>받은 초대 코드 입력하기 (분리하기) </div>
-      <div>진행중 / 종료</div>
+      <header className={BattleStyles.header}>
+        <p className={BattleStyles.headerTitle}>배틀 목록</p>
+      </header>
+      <div className={BattleStyles.wrapper}>
+        <Link to="/battle/code" state={{}}>
+          <div className={BattleStyles.inviteCodeInput}>
+            <img
+              className={BattleStyles.inviteImage}
+              src={Invite}
+              alt="초대장"
+            />
+            <p>받은 초대 코드 입력하기</p>
+          </div>
+        </Link>
 
-      <ul className="container">
-        {data &&
-          data
-            .filter((item) => item.status === 1)
-            .map((item, index) => (
-              <li key={index}>
-                <img src={`/images/${item.imgPath}`} alt="profileImg" />
-                <div>
-                  <h3 onClick={() => handleBattleItemClick(item)}>
-                    {item.name}
-                  </h3>
-                  <span>
-                    {' '}
-                    D-day :
-                    {getDday(new Date(item.startDate), new Date(item.endDate))}
-                  </span>
+        <div className={BattleStyles.battleStatus}>
+          <div
+            className={`${BattleStyles.battleProgress} ${
+              activeTab === 'progress' && BattleStyles.active
+            }`}
+            onClick={() => setActiveTab('progress')}
+          >
+            진행중
+          </div>
+          <div
+            className={`${BattleStyles.battleEnd} ${
+              activeTab === 'end' && BattleStyles.active
+            }`}
+            onClick={() => setActiveTab('end')}
+          >
+            종료
+          </div>
+        </div>
+
+        <ul>
+          {data &&
+            data
+              .filter(
+                (item) =>
+                  (activeTab === 'progress' &&
+                    item.status === 1 &&
+                    getDday(new Date(item.startDate), new Date(item.endDate)) >
+                      0) ||
+                  (activeTab === 'end' &&
+                    item.status === 1 &&
+                    getDday(
+                      new Date(item.startDate),
+                      new Date(item.endDate),
+                    ) === 0),
+              )
+              .map((item, index) => (
+                <div onClick={() => handleBattleItemClick(item)}>
+                  <li key={item.battleCode} className={BattleStyles.container}>
+                    <img
+                      className={BattleStyles.profileImg}
+                      src={profileImg}
+                      alt={'아바타 이미지'}
+                    />
+                    <div className={BattleStyles.content}>
+                      <div className={BattleStyles.battleTitle}>
+                        {item.name}
+                      </div>
+                      <div className={BattleStyles.dDay}>
+                        <span>
+                          {' '}
+                          {getDday(
+                            new Date(item.startDate),
+                            new Date(item.endDate),
+                          ) === 0
+                            ? '종료'
+                            : `D-${getDday(
+                                new Date(item.startDate),
+                                new Date(item.endDate),
+                              )}`}
+                        </span>
+                      </div>
+                      {item.playerList[0].liveStatus === 1 ||
+                      item.playerList[1].liveStatus === 1 ? (
+                        <img
+                          className={BattleStyles.liveImg}
+                          src={liveImg}
+                          alt={'라이브 표시 이미지'}
+                        />
+                      ) : null}
+                    </div>
+                    <div className={BattleStyles.partner}>
+                      {item.playerList[0].nickname}
+                      &nbsp; vs &nbsp;
+                      {item.playerList[1].nickname}
+                    </div>
+                    <div className={BattleStyles.triggerCnt}>
+                      자극자 {item.triggerCnt}명
+                    </div>
+                    {item.userCode === userCode && (
+                      <button onClick={() => onDeleteBattle(item.battleCode)}>
+                        배틀 삭제
+                      </button>
+                    )}
+                  </li>
                 </div>
-                <div>{item.nickname}가 생성</div>
-                <div>자극자 수 : {item.triggerCnt}</div>
-                <div>
-                  {getDday(new Date(item.startDate), new Date(item.endDate)) ===
-                  0
-                    ? '배틀 상태는 종료'
-                    : '배틀 상태는 진행중'}
-                </div>
-                {item.userCode === userCode && (
-                  <button onClick={() => onDeleteBattle(item.battleCode)}>
-                    배틀 삭제
-                  </button>
-                )}
-              </li>
-            ))}
-      </ul>
-      {/* 검토중인 배틀방들 */}
-      <ul className="container">
-        {data &&
-          data
-            .filter((item) => item.status === 0)
-            .map((item, index) => (
-              <li key={index}>
-                <img src={`/images/${item.imgPath}`} alt="profileImg" />
-                <div>
-                  <Link
-                    to="/battle/create"
-                    state={{
-                      battleCode: item.battleCode,
-                    }}
-                  >
-                    <h3>{item.name}</h3>
-                  </Link>
-                  <span>
-                    {' '}
-                    D-day :
-                    {getDday(new Date(item.startDate), new Date(item.endDate))}
-                  </span>
-                </div>
-                <div>{item.nickname}가 생성</div>
-                <div>자극자 수 : {item.triggerCnt}</div>
-                <div>배틀 상태는 검토중</div>
-                {item.userCode === userCode && (
-                  <button onClick={() => onDeleteBattle(item.battleCode)}>
-                    배틀 삭제
-                  </button>
-                )}
-              </li>
-            ))}
-      </ul>
-      <button className={BattleStyles.btnRed} onClick={onCreateBattle}>
-        1 : 1배틀 만들기
-      </button>
+              ))}
+        </ul>
+
+        <ul>
+          {data &&
+            data
+              .filter((item) => item.status === 0 && activeTab === 'progress')
+              .map((item, index) => (
+                <Link
+                  to="/battle/create"
+                  state={{
+                    battleCode: item.battleCode,
+                  }}
+                >
+                  <li key={item.battleCode} className={BattleStyles.container}>
+                    <img
+                      className={BattleStyles.profileImg}
+                      src={profileImg}
+                      alt={'아바타 이미지'}
+                    />
+
+                    <div className={BattleStyles.content}>
+                      <div className={BattleStyles.battleTitle}>
+                        {item.name}
+                      </div>
+                      <div className={BattleStyles.dDay}>
+                        <span> 검토중</span>
+                      </div>
+                    </div>
+                    <div className={BattleStyles.partner}>계약서 검토중</div>
+
+                    {item.userCode === userCode && (
+                      <img
+                        className={BattleStyles.trashCan}
+                        src={trashCan}
+                        alt={'삭제 이미지'}
+                        onClick={() => onDeleteBattle(item.battleCode)}
+                      />
+                    )}
+                  </li>
+                </Link>
+              ))}
+        </ul>
+        <div className={BattleStyles.btnRed} onClick={onCreateBattle}>
+          1 : 1배틀 만들기
+        </div>
+      </div>
       <Footer />
     </>
   );
