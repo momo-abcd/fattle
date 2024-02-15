ec83181ae7d2aa2e0824ae4617c18e074f9d68df
정창휘 <jch9937@gmail.com>
2024-02-15 17:13:12 +0900

    fix: 디테일 수정

--- FE/src/config.js
+++ FE/src/config.js
@@ -1,2 +1,2 @@
-const BASE_URL = 'https://i10e106.p.ssafy.io/api';
+const BASE_URL = 'http://localhost:8000/api';
 export default BASE_URL;
--- FE/src/pages/battle/BattleDetail.js
+++ FE/src/pages/battle/BattleDetail.js
@@ -47,7 +47,6 @@ const BattleDetail = (props) => {
 
   // 넘기기 데이터 변수
   const [modalData, setModalData] = useState(null);
-
   useEffect(() => {
     if (state === null) {
       navigate('/battle');
@@ -58,6 +57,7 @@ const BattleDetail = (props) => {
         const res = await getBattleInfo(state.battleCode);
         setData(res.data);
 
+        // const startDate = new Date(res.data.startDate);
         const endDate = new Date(res.data.endDate);
         const difference = endDate - new Date();
         const days = Math.floor(difference / (1000 * 60 * 60 * 24));
@@ -68,7 +68,7 @@ const BattleDetail = (props) => {
           (difference % (1000 * 60 * 60)) / (1000 * 60),
         );
         setTimeRemaining({ days, hours, minutes });
-
+        // 남은 시간이 0이 되면 배틀 종료
         if (difference <= 0) {
           setBattleEnded(true);
           const finalWeight = calculateFinalWeight();
@@ -82,6 +82,10 @@ const BattleDetail = (props) => {
             data.playerList[0].userCode,
             finalWeight,
           );
+          // 배틀 종료 API 호출
+          // await finishBattle(state.battleCode);
+          // 승리자 페이지로 이동
+          // navigate('/battle');
           await finishBattleWeight();
         }
       } catch (error) {
@@ -91,11 +95,13 @@ const BattleDetail = (props) => {
 
     fetchData();
     (async () => {
+      // 배틀 정보 얻기
       try {
         const res = await getBattleInfo(state.battleCode);
         setBattleInfo(res.data);
         setPlayerList(res.data.playerList);
 
+        // 배틀 식단 게시판 리스트 얻기
         const boardListRes = await getBattleBoardList(state.battleCode);
         setFoodBoardList(boardListRes.data.list);
         console.log(boardListRes.data.list);
@@ -107,12 +113,14 @@ const BattleDetail = (props) => {
   }, [state, navigate]);
 
   const calculateFinalWeight = () => {
+    // data가 존재하고 data.playerList도 존재하며 data.playerList[0]도 존재할 때에만 계산 수행
     if (data && data.playerList && data.playerList.length > 0) {
+      // 여기에 실제로 몸무게 계산
       const finalWeight =
         data.playerList[0].beforeWeight - data.playerList[0].afterWeight;
       return finalWeight;
     }
-    return 0;
+    return 0; // 위의 조건 중 하나라도 해당되지 않으면 0을 반환
   };
 
   const handleToggleCodeView = () => {
@@ -130,7 +138,7 @@ const BattleDetail = (props) => {
   if (!data) {
     return <div>Loading...</div>;
   }
-
+  // METHOD
   const handleClose = () => setFoodModalShow(false);
   const handleShow = () => setFoodModalShow(true);
   const showFoodBoard = (
@@ -151,7 +159,6 @@ const BattleDetail = (props) => {
     });
     handleShow();
   };
-
   return (
     <div className={styles.container}>
       <header className={styles.header}>
@@ -175,108 +182,153 @@ const BattleDetail = (props) => {
                 {getDday(new Date(data.startDate), new Date(data.endDate))}
               </div>
               <div className={styles.battleTitle}>{data.battleName}</div>
-
-              {1
-                ? data.playerList[1] &&
-                  data.playerList[1].userCode === userCode && (
-                    <>
-                      <div className={styles.profileImages}>
-                        <img src={`/${data.playerList[1].profileImgPath}`} />
-                        <span className={styles.versus}>vs</span>
-                        <img src={`/${data.playerList[0].profileImgPath}`} />
-                      </div>
-                      <div className={styles.columns}>
-                        <div className={styles.rows}>
-                          <div className={styles.name}>
-                            {data.playerList[1].nickname}
-                          </div>
-                          <div className={styles.now}>지금까지</div>
-                          <div className={styles.score}>
-                            {data.playerList[1].foodPoint +
-                              data.playerList[1].foodUserPoint +
-                              data.playerList[1].goalPoint +
-                              data.playerList[1].livePoint +
-                              data.playerList[1].liveUserPoint +
-                              data.playerList[1].questPoint}
-                            점
-                          </div>
-                          <div className={styles.goal}>
-                            목표 : {data.playerList[1].goalWeight} kg
-                          </div>
+              {!data.playerList[0] && (
+                <>
+                  <div className={styles.profileImages}>
+                    {!data.playerList[0] && (
+                      <>
+                        <img src={nullImage} alt="빈 프로필" />
+                      </>
+                    )}
+                    <span className={styles.versus}>vs</span>
+                    {!data.playerList[1] && (
+                      <>
+                        <img src={nullImage} alt="빈 프로필" />
+                      </>
+                    )}
+                  </div>
+                  <div style={{ color: 'white' }}>내기자를 설정해주세요!!</div>
+                </>
+              )}
+              {data.playerList[1] &&
+                data.playerList[1].userCode === userCode && (
+                  <>
+                    <div className={styles.profileImages}>
+                      <img
+                        src={`/${data.playerList[1].profileImgPath}`}
+                        // alt="나의 프로필 이미지"
+                      />
+                      <span className={styles.versus}>vs</span>
+                      <img
+                        src={`/${data.playerList[0].profileImgPath}`}
+                        // alt="상대방 프로필 이미지"
+                      />
+                    </div>
+                    <div className={styles.columns}>
+                      <div className={styles.rows}>
+                        {/* {data.playerList[0].liveStatus === 0 ||
+                        data.playerList[1].liveStatus === 1 ? (
+                          <img
+                            className={BattleStyles.liveImage}
+                            src={liveImg}
+                            alt={'라이브 표시 이미지'}
+                          />
+                        ) : null} */}
+                        <div className={styles.name}>
+                          {data.playerList[1].nickname}
                         </div>
-                        <div className={styles.rows}>
-                          <div className={styles.name}>
-                            {data.playerList[0].nickname}
-                          </div>
-                          <div className={styles.now}>지금까지</div>
-                          <div className={styles.score}>
-                            {data.playerList[0].foodPoint +
-                              data.playerList[0].foodUserPoint +
-                              data.playerList[0].goalPoint +
-                              data.playerList[0].livePoint +
-                              data.playerList[0].liveUserPoint +
-                              data.playerList[0].questPoint}
-                            점
-                          </div>
-                          <div className={styles.goal}>
-                            목표 : {data.playerList[0].goalWeight} kg
-                          </div>
+                        <div className={styles.now}>지금까지</div>
+                        <div className={styles.score}>
+                          {data.playerList[1].foodPoint +
+                            data.playerList[1].foodUserPoint +
+                            data.playerList[1].goalPoint +
+                            data.playerList[1].livePoint +
+                            data.playerList[1].liveUserPoint +
+                            data.playerList[1].questPoint}
+                          점
                         </div>
+                        <divv className={styles.goal}>
+                          목표 : {data.playerList[1].goalWeight}
+                          kg
+                        </divv>
                       </div>
-                    </>
-                  )
-                : data.playerList[0] &&
-                  data.playerList[0].userCode === userCode && (
-                    <>
-                      <div className={styles.profileImages}>
-                        <img src={`/${data.playerList[0].profileImgPath}`} />
-                        <span className={styles.versus}>vs</span>
-                        <img src={`/${data.playerList[1].profileImgPath}`} />
-                      </div>
-                      <div className={styles.columns}>
-                        <div className={styles.rows}>
-                          <div className={styles.name}>
-                            {data.playerList[0].nickname}
-                          </div>
-                          <div className={styles.now}>지금까지</div>
-                          <div className={styles.score}>
-                            {data.playerList[0].foodPoint +
-                              data.playerList[0].foodUserPoint +
-                              data.playerList[0].goalPoint +
-                              data.playerList[0].livePoint +
-                              data.playerList[0].liveUserPoint +
-                              data.playerList[0].questPoint}
-                            점
-                          </div>
-                          <div className={styles.goal}>
-                            목표 : {data.playerList[0].goalWeight} kg
-                          </div>
+                      <div className={styles.rows}>
+                        <div className={styles.name}>
+                          {data.playerList[0].nickname}
                         </div>
-                        <div className={styles.rows}>
-                          <div className={styles.name}>
-                            {data.playerList[1].nickname}
-                          </div>
-                          <div className={styles.now}>지금까지</div>
-                          <div className={styles.score}>
-                            {data.playerList[1].foodPoint +
-                              data.playerList[1].foodUserPoint +
-                              data.playerList[1].goalPoint +
-                              data.playerList[1].livePoint +
-                              data.playerList[1].liveUserPoint +
-                              data.playerList[1].questPoint}
-                            점
-                          </div>
-                          <div className={styles.goal}>
-                            목표 : {data.playerList[1].goalWeight} kg
-                          </div>
+                        <div className={styles.now}>지금까지</div>
+                        <div className={styles.score}>
+                          {data.playerList[0].foodPoint +
+                            data.playerList[0].foodUserPoint +
+                            data.playerList[0].goalPoint +
+                            data.playerList[0].livePoint +
+                            data.playerList[0].liveUserPoint +
+                            data.playerList[0].questPoint}
+                          점
+                        </div>
+                        <div className={styles.goal}>
+                          목표 : {data.playerList[0].goalWeight}
+                          kg
                         </div>
                       </div>
-                    </>
-                  )}
+                    </div>
+                  </>
+                )}
+              {data.playerList[0] &&
+                data.playerList[0].userCode === userCode && (
+                  <>
+                    <div className={styles.profileImages}>
+                      <img
+                        src={`/${data.playerList[0].profileImgPath}`}
+                        // alt="나의 프로필 이미지"
+                      />
+                      <span className={styles.versus}>vs</span>
+                      <img
+                        src={`/${data.playerList[1].profileImgPath}`}
+                        // alt="상대방 프로필 이미지"
+                      />
+                    </div>
+                    <div className={styles.columns}>
+                      <div className={styles.rows}>
+                        {/* {data.playerList[0].liveStatus === 1 ||
+                        data.playerList[1].liveStatus === 1 ? (
+                          <img
+                            className={BattleStyles.liveImage}
+                            src={liveImg}
+                            alt={'라이브 표시 이미지'}
+                          />
+                        ) : null} */}
+                        <div className={styles.name}>
+                          {data.playerList[0].nickname}
+                        </div>
+                        <div className={styles.now}>지금까지</div>
+                        <div className={styles.score}>
+                          {data.playerList[0].foodPoint +
+                            data.playerList[0].foodUserPoint +
+                            data.playerList[0].goalPoint +
+                            data.playerList[0].livePoint +
+                            data.playerList[0].liveUserPoint +
+                            data.playerList[0].questPoint}
+                          점
+                        </div>
+                        <div className={styles.goal}>
+                          목표 : {data.playerList[0].goalWeight}
+                          kg
+                        </div>
+                        <div className={styles.name}>
+                          {data.playerList[1].nickname}
+                        </div>
+                        <div className={styles.now}>지금까지</div>
+                        <div className={styles.score}>
+                          {data.playerList[1].foodPoint +
+                            data.playerList[1].foodUserPoint +
+                            data.playerList[1].goalPoint +
+                            data.playerList[1].livePoint +
+                            data.playerList[1].liveUserPoint +
+                            data.playerList[1].questPoint}
+                          점
+                        </div>
+                        <div className={styles.goal}>
+                          목표 : {data.playerList[1].goalWeight}
+                          kg
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                )}
             </div>
           )}
         </div>
-
         {!battleEnded && (
           <div className={styles.livebutton}>
             {playerList &&
@@ -318,7 +370,6 @@ const BattleDetail = (props) => {
               ))}
           </div>
         )}
-
         <div>
           {battleEnded && (
             <div onClick={handleViewResult} className={styles.goResult}>
@@ -328,6 +379,7 @@ const BattleDetail = (props) => {
         </div>
 
         <div className={styles.graph}>
+          {/* <h3>열량 비교</h3> */}
           <BattleDetailCal
             userCode={data.playerList[0].userCode}
             secondPlayeruserCode={data.playerList[1].userCode}
@@ -338,6 +390,7 @@ const BattleDetail = (props) => {
           <Loading />
         ) : (
           <div className={styles.foodAlbum}>
+            {/* 식단 앨범 */}
             <div className={styles.dietAlbum}>
               <div className={styles.bTitle}>식단 앨범</div>
               <div className={styles.line}></div>
